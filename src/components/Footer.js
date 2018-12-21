91e56af134bb329a3958cfb5b69cf71c878a315c
ylzhu000 <yzhu000@citymail.cuny.edu>
2018-12-21 16:16:25 -0500

    changed bottom quote

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ export default class Footer extends Component {
 	          <div className="container">
 	            <div className="row" id="quote-row-container">
 	              <div className="col s12">
-	                <p className="grey-text text-lighten-4 right"><i className="material-icons">format_quote</i>YOU RENEW YOURSELF EVERY DAY. SOMETIMES YOU'RE SUCESSFUL, SOMETIMES YOUR NOT, BUT IT'S THE AVERAGE THAT COUNTS.<i className="material-icons">format_quote</i></p>
+	                <p className="grey-text text-lighten-4 right"><i className="material-icons">format_quote</i>TALK IS CHEAP. SHOW ME THE CODE.<i className="material-icons">format_quote</i></p>
 	              </div>
-	              <p className="right">-- Satya Nadella</p>
+	              <p className="right">-- Linus Torvalds</p>
 	            </div>
 	          </div>
 	          <div className="footer-copyright">
